77247371d8fc974754696489968ac3549f62b188
Olivier Hang <hang.sengolivier@gmail.com>
2021-01-28 20:57:25 +0100

    validation des donnees du formulaire lors du submit + changement de la langue dans 'translation.yaml'

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -11,6 +11,7 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
 
 class RegisterType extends AbstractType
 {
@@ -19,24 +20,28 @@ class RegisterType extends AbstractType
         $builder
             ->add('firstname', TextType::class, [
                 'label' => 'Votre prénom',
+                'constraints' => new Length(null, 2, 30),
                 'attr' => [
                     'placeholder' => 'Merci de saisir votre prénom'
                 ]
             ])
             ->add('lastname', TextType::class, [
                 'label' => 'Votre nom',
+                'constraints' => new Length(null, 2, 30),
                 'attr' => [
                     'placeholder' => 'Merci de saisir votre nom'
                 ]
             ])
             ->add('email', EmailType::class, [
                 'label' => 'Votre email',
+                'constraints' => new Length(null, 2, 30),
                 'attr' => [
                     'placeholder' => "Merci de saisir votre email"
                 ]
             ])
             ->add('password', RepeatedType::class, [
                 'type' => PasswordType::class,
+                'constraints' => new Length(null, 2, 60),
                 'invalid_message' => 'Le mot de passe et la confirmation doivent être identique.',
                 'label' => 'Votre mot de passe',
                 'required' => true,
