96b89e6b0f6f81019361d6ff41a2e0f0333db3fa
Olivier Hang <hang.sengolivier@gmail.com>
2021-01-29 21:09:15 +0100

    creation de espace utilisateur

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -14,9 +14,10 @@ class SecurityController extends AbstractController
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
+        // Si y a un User déjà connecter, redirection vers account
+        if ($this->getUser()) {
+            return $this->redirectToRoute('account');
+        }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
